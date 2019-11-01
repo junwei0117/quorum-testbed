0c5c07891c5ec24c8727242be3f3675ec0de3504
junwei0117 <v860117@gmail.com>
2019-11-01 22:10:44 +0800

    Refactor and remove the useless code

--- zether/packages/example/index.js
+++ zether/packages/example/index.js
@@ -4,8 +4,8 @@ const ZSC = require("../contract-artifacts/artifacts/ZSC.json");
 const Deployer = require('./deployer.js');
 const Provider = require('./provider.js');
 
-(async () => {
-    var provider = new Provider("ws://localhost:23000");
+const run = async () => {
+    const provider = new Provider("ws://localhost:23000");
     const web3 = new Web3(await provider.getProvider());
 
     web3.transactionConfirmationBlocks = 1;
@@ -25,4 +25,6 @@ const Provider = require('./provider.js');
     await client.withdraw(1000);
     client.friends.add("Alice", ['0x0eaadaaa84784811271240ec2f03b464015082426aa13a46a99a56c964a5c7cc', '0x173ce032ad098e9fcbf813696da92328257e58827f3600b259c42e52ff809433']);
     await client.transfer('Alice', 1000);
-})().catch(console.error);
+};
+
+run();
